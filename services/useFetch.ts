a711b0c5dcad19b3cd5ffefc4955129215e61071
Siddharth Gupta <sgupta@data-vector.com>
2025-03-23 16:51:58 +0530

    first commit

---
+++ services/useFetch.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
+    const [data, setData] = useState<T | null>(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<Error | null>(null);
+
+    const fetchData = async() => {
+        try {
+            setLoading(true);
+            setError(null);
+
+            setData(await fetchFunction());
+
+        } catch (error) {
+            //@ts-ignore 
+            console.error(error);
+            setError(error instanceof Error ? error : new Error("An Error Occurred"));
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const reset = () => {
+        setData(null);
+        setLoading(false);
+        setError(null);
+    }
+
+    useEffect(() => {
+        if (autoFetch) {
+            fetchData();
+        }
+    }, [])
+
+    return {
+        data,
+        loading,
+        error,
+        refetch:fetchData,
+        reset
+    }
+}
+
+export default useFetch;
